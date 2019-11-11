075764c01aee15db28796b1a3326963852aafaf5
zaimsyaddad99 <zaimibnusyam@gmail.com>
2019-11-11 08:59:10 -0800

    start

--- main.pl
+++ main.pl
@@ -2,3 +2,9 @@
     TOKEMON GAME*/
 
 /*Game loop, begin if user types start */
+
+start :-
+        write('==========>>>>>TOKEMON<<<<<==========\n===============PROLOG===============\n'),
+        write('\nGotta catch em all!\n'),
+        write('\n'),
+        write('Hello there! Welcome to the world of Tokemon! My name is Aril!\nPeople call me the Tokemon Professor! This world is inhabited by\ncreatures called Tokemon! There are hundreds of Tokemon loose in\n Labtek 5! You can catch them all to get stronger, but what I am\n really interested in are the 2 legendary Tokemons, Icanmon dan \nSangemon. If you can defeat or capture all those Tokemons I will\n not kill you.'), !.
