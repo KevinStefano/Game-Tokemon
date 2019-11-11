98724a63f4343f875510ccba9b64be61dd2b2d62
Wildan Zaim Syaddad <52845131+zaimsyaddad99@users.noreply.github.com>
2019-11-11 14:15:43 +0700

    Update main.pl

--- main.pl
+++ main.pl
@@ -12,6 +12,31 @@ start :-
         write('\nGotta catch em all!\n'),
         write('\n'),
         write('Hello there! Welcome to the world of Tokemon! My name is Aril!\nPeople call me the Tokemon Professor! This world is inhabited by\ncreatures called Tokemon! There are hundreds of Tokemon loose in\n Labtek 5! You can catch them all to get stronger, but what I am\n really interested in are the 2 legendary Tokemons, Icanmon dan \nSangemon. If you can defeat or capture all those Tokemons I will\n not kill you.\n\n'), 
-        write('Available commands:\n1. start. -- start the game!\n2. help. -- show available commands \n3. quit. -- quit the game\n4. n. s. e. w. -- move\n5. map. -- look at the map\n6. heal -- cure Tokemon in inventory if in gym center\n7. status. -- show your status\n8. save(Filename). -- save your game\n9. load(Filename). -- load previously saved game\n'),
+        write('Available commands:\n'),
+        write('1. start. -- start the game!\n'),
+        write('2. help. -- show available commands\n'),
+        write('3. quit. -- quit the game\n'),
+        write('4. n. s. e. w. -- move\n'),
+        write('5. map. -- look at the map\n'),
+        write('6. heal -- cure Tokemon in inventory if in gym center\n'),
+        write('7. status. -- show your status\n'),
+        write('8. save(Filename). -- save your game\n'),
+        write('9. load(Filename). -- load previously saved game\n'),
         write('\nLegends:\n- X = Pagar\n- P = Player\n- G = Gym\n'),!.
-
+help :-
+        write('Available commands:\n'),
+        write('1. start. -- start the game!\n'),
+        write('2. help. -- show available commands\n'),
+        write('3. quit. -- quit the game\n'),
+        write('4. n. s. e. w. -- move\n'),
+        write('5. map. -- look at the map\n'),
+        write('6. heal -- cure Tokemon in inventory if in gym center\n'),
+        write('7. status. -- show your status\n'),
+        write('8. save(Filename). -- save your game\n'),
+        write('9. load(Filename). -- load previously saved game\n'),!.
+status :-
+        write('Your Tokemon:\n'),
+        /*function liat tokemon disini*/
+        write('Your enemy:\n'),!.
+        /*liat wild tokemon*/
+        
