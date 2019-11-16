18bb9da19b262b88c310aafa58727ced3ed4ab21
zaimsyaddad99 <zaimibnusyam@gmail.com>
2019-11-16 00:05:59 -0800

    save bisa, load belum

--- main.pl
+++ main.pl
@@ -4,11 +4,12 @@
 :-include('tokemon.pl').
 :-include('tokemon_evolution').
 :-include('map.pl').
-
+:- dynamic(gameMain/1).	
 
 /*Game begin if user types start */
 
-start :-
+start :- 
+        asserta(gameMain(1)),
         write('==========>>>>>TOKEMON<<<<<==========\n===============PROLOG===============\n'),
         story, 
         help,
@@ -35,7 +36,8 @@ run(w):- w,nl,!.
 run(n) :- n,nl,!.
 run(load):-write('masukkan nama file:'),nl,read(X),loads(X),!.
 run(start):-write('Kamu sudah berada pada game.'),nl,!.
-
+run(save):-write('masukkan nama file:'),nl,read(X),save(X),
+                nl,write('Berhasil disimpan!'),nl,!.
 
 legends:- write('\nLegends:\n- X = Pagar\n- P = Player\n- G = Gym\n'),nl.
 
@@ -67,9 +69,7 @@ status :-
         write('Your enemy:\n'),!.
         /*liat wild tokemon*/
 
-loads(_):-
-        gameMain(_),
-        write('Kamu tidak bisa melakukan perintah ini ketika sedang berada pada game.'),nl,!.
+
 loads(FileName):-
         \+file_exists(FileName),
         write('File tidak ditemukan'),nl,!.
@@ -78,3 +78,51 @@ loads(FileName):-
         read_file_lines(Str,Lines),
         close(Str),
         assertaList(Lines),!.
+
+save(_):-
+        \+gameMain(_),
+        write('Perintah ini hanya bisa dilakukan ketika kamu berada pada game.'),!.
+save(FileName):-
+        tell(FileName),
+
+        currLoc(X,Y),
+        write(currLoc(X,Y)),write('.'),nl,
+        gameMain(GM),
+        write(gameMain(GM)),write('.'),nl,
+        told,!.
+
+
+/* Read dari file eksternal */
+readData(S,[]) :-
+	at_end_of_stream(S), !.
+
+readData(S,[X1|Tail]) :-
+	get_char(S,X1),
+	readData(S,Tail).
+
+baca_file(NamaFile,Isi) :-
+	open(NamaFile,read,S),
+	repeat,
+	readData(S,Isi),
+	close(S),!.
+
+/* Membaca file menjadi list of lines */
+read_file_lines(Stream,[]) :-
+    at_end_of_stream(Stream).
+
+read_file_lines(Stream,[X|L]) :-
+    \+ at_end_of_stream(Stream),
+    read(Stream,X),
+    read_file_lines(Stream,L).
+/*-----------------------------*/
+/* Write ke file eksternal */
+writeData(_,[]) :- !.
+writeData(S,[X1|Tail]) :-
+	write(S,X1),
+	writeData(S,Tail).
+
+write_list(NamaFile,L) :-
+	open(NamaFile,write,S),
+	repeat,
+	writeData(S,L),
+	close(S).
