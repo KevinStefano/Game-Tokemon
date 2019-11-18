df9e4838de166def29bdd92b262185fb0d2f9680
zaimsyaddad99 <zaimibnusyam@gmail.com>
2019-11-17 22:19:22 -0800

    heal loop for all tokemon in storage

--- gym.pl
+++ gym.pl
@@ -1,12 +1,12 @@
 heal:- 
     currLoc(8,4),
-    forall(sembuhkanTokemon(Tokemon,Health),nl),
+    forall(storage(Tokemon,Health),(sembuhkanTokemon(Tokemon,Health),nl)),
     write('Semua Tokemon dalam inventory sudah sembuh.'),nl,!.
 
 /*Menyembuhkan Tokemon*/
 /*Jika Health tidak full */
 sembuhkanTokemon(Tokemon,Health):-
 	delStorage(Tokemon,Health),
-	tokemon(_,Tokemon,_,HealthMax,_,_,_,_),
-	addStorage(Tokemon,HealtMax),!.
+    tokemon(_,Tokemon,_,HealthMax,_,_,_,_),
+	addStorage(Tokemon,HealthMax),!.
 
